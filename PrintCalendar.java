5058287ff589f22e1fbe103996d75b1e9a25a71a
s9202259 <s9202259@yahoo.com.tw>
2016-03-14 14:23:10 +0800

    新增method
    新增了
    printMonth()
    printMonthTitle()
    getMonthName()

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -1,5 +1,5 @@
 
-
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class PrintCalendar {
@@ -19,5 +19,43 @@ public class PrintCalendar {
 			System.out.println("Please enter again.");
 			month = input.nextInt();
 		}
+
+		GregorianCalendar g = new GregorianCalendar(); 
+        g.set(GregorianCalendar.YEAR, year);
+        g.set(GregorianCalendar.MONTH, month);
+
+        System.out.println("" + year + "   " + month);
+        
+        printMonth(year,month);
+	}
+	public static void printMonth(int year, int month) {
+		printMonthTitle(year, month);
+	}
+
+
+	public static void printMonthTitle(int year, int month) {
+		System.out.println("	" + getMonthName(month) + 
+				"	" + year);
+		System.out.println("---------------------------");
+		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+	}
+	
+	public static String getMonthName(int month) {
+		String monthName = "";
+		switch(month) {
+			case 1: monthName = "January"; break;
+			case 2: monthName = "February"; break;
+			case 3: monthName = "March"; break;
+			case 4: monthName = "April"; break;
+			case 5: monthName = "May"; break;
+			case 6: monthName = "June"; break;
+			case 7: monthName = "July"; break;
+			case 8: monthName = "August"; break;
+			case 9: monthName = "September"; break;
+			case 10: monthName = "October"; break;
+			case 11: monthName = "November"; break;
+			case 12: monthName = "December";
+		}
+		return monthName;
 	}
 }
